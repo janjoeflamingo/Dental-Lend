151f1a51d2b1fcea237b178fbccc1a4304c3d20b
janjoeflamingo <van.loner@mail.ru>
2019-05-16 13:24:24 +0400

    added project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import style from "./assets/styles/main.scss";
+
+
+//*****************************************************************
+
+
+//               remove comment to support pwa
+
+
+// import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+//
+// if ('serviceWorker' in navigator) {
+//     const registration = runtime.register();
+// }
+
+
+
+//*****************************************************************
+
